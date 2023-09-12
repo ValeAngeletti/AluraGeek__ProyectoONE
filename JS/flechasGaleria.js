7eac48a87689785f6dc611ccb53c550fe9129529
Vale Angeletti <valeangelett11@gmail.com>
2023-09-12 19:04:11 -0300

    Estilos de todas las galerias

--- JS/flechasGaleria.js
+++ JS/flechasGaleria.js
@@ -1,11 +1,42 @@
 document.querySelector('.carrusel__flecha--izquierda').addEventListener('click', function () {
     const galeria = document.querySelector('.acces__galeria');
-    galeria.scrollLeft -= 200; // Ajusta la cantidad de desplazamiento según tus necesidades
+    galeria.scrollLeft -= 200; 
 });
 
 document.querySelector('.carrusel__flecha--derecha').addEventListener('click', function () {
     const galeria = document.querySelector('.acces__galeria');
-    galeria.scrollLeft += 200; // Ajusta la cantidad de desplazamiento según tus necesidades
+    galeria.scrollLeft += 200; 
 });
+
+document.querySelector('.carrusel__flecha--izquierda--pants').addEventListener('click', function () {
+    const galeria = document.querySelector('.pantalones__galeria');
+    galeria.scrollLeft -= 200; 
+});
+
+document.querySelector('.carrusel__flecha--derecha--pants').addEventListener('click', function () {
+    const galeria = document.querySelector('.pantalones__galeria');
+    galeria.scrollLeft += 200; 
+});
+
+document.querySelector('.carrusel__flecha--izquierda--rem').addEventListener('click', function () {
+    const galeria = document.querySelector('.remeras__galeria');
+    galeria.scrollLeft -= 200; 
+});
+
+document.querySelector('.carrusel__flecha--derecha--rem').addEventListener('click', function () {
+    const galeria = document.querySelector('.remeras__galeria');
+    galeria.scrollLeft += 200; 
+});
+
+document.querySelector('.carrusel__flecha--izquierda--zap').addEventListener('click', function () {
+    const galeria = document.querySelector('.zapatillas__galeria');
+    galeria.scrollLeft -= 200; 
+});
+
+document.querySelector('.carrusel__flecha--derecha--zap').addEventListener('click', function () {
+    const galeria = document.querySelector('.zapatillas__galeria');
+    galeria.scrollLeft += 200; 
+});
+
 
 console.log("funcionando")
